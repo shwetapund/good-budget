e1108114f33b1bd0dad5e686cf90732f4bb099ee
shwetapund <pundshweta2020@gmail.com>
2023-11-26 00:13:59 -0800

    feat: create Schema and model

---
+++ server/models/Transactions.js
@@ -0,0 +1,28 @@
+import {Schema, model} from "mongoose";
+
+const transactionalSchema = new Schema({
+    amount: {
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['credit', 'debit'],
+        required: true
+    },
+    category: {
+        type:String,
+        enum: ['food','entertainement','shopping',"rent",'travel','education','other'],
+        default:other,
+    },
+    description: {
+        type:String,
+    }
+},
+{
+    timestamps:true,
+})
+
+const Transaction = model('Transaction',transactionalSchema);
+
+export default Transaction 
