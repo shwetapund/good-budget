484c9580a967e6d7409371720424a2376472a8ec
shwetapund <pundshweta2020@gmail.com>
2023-11-27 20:40:13 -0800

    create User model and Schema

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+import {Schema, model} from 'mongoose'
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    address:{
+        type:String,
+    },
+    mobileNo:{
+        type:Number,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    }
+})
+
+const User = model('User',userSchema)
+
+export default User
